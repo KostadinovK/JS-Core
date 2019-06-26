8a49115ab3a0355e346ea4f8b94cd18af998887d
KostadinovK <kostadinovk2001@gmail.com>
2019-06-26 08:34:19 +0300

    Fixed a typo in task 05 from the lab to lecture 05

--- 02-JS Advanced/05-Object Composition/Lab/05-Sum.js
+++ 02-JS Advanced/05-Object Composition/Lab/05-Sum.js
@@ -7,7 +7,7 @@ function solve(){
         init: (selector1, selector2, resultSelector) => {
             element1 = document.querySelector(selector1);
             element2 = document.querySelector(selector2);
-            resultSelector = document.querySelector(resultSelector);
+            result = document.querySelector(resultSelector);
         },
         add: () => result.value = +element1.value + +element2.value,
         subtract: () => result.value = +element1.value - +element2.value
